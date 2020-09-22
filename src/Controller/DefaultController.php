28d193b4052c5a42f757656a348131929942eec2
bashokusan <pierresimonnet1@gmail.com>
2020-09-23 00:58:25 +0200

    install phpstan

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="app_default_page")
+     * @return Response
      */
     public function index()
     {
